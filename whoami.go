cdbd805537cf66a907e0b223fa6abbe945a11d0c
wanghaibo <wanghaibo@myhexin.com>
2017-11-13 16:32:07 +0800

    add ips

--- whoami.go
+++ whoami.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -15,10 +17,28 @@ func main() {
 
 	fmt.Fprintf(os.Stdout, "Listening on :%s\n", port)
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		for _, e := range os.Environ() {
-			fmt.Fprintf(os.Stdout, "%s\n", e)
-			fmt.Fprintf(w, "%s\n", e)
+		addrs, err := net.InterfaceAddrs()
+
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
 		}
+
+		var ips []string
+
+		for _, address := range addrs {
+
+			// 检查ip地址判断是否回环地址
+			if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+				if ipnet.IP.To4() != nil {
+					ips = append(ips, ipnet.IP.String())
+				}
+
+			}
+		}
+		ipStr := strings.Join(ips, " ")
+		fmt.Fprintf(os.Stdout, "my ips is:%s\n", ipStr)
+		fmt.Fprintf(w, "my ips is:%s\n", ipStr)
 	})
 
 	log.Fatal(http.ListenAndServe(":"+port, nil))
